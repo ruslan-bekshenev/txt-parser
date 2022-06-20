52092bfaa63ef5440aaccc681a647c59762697ea
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-06-21 03:54:18 +0500

    done parse

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const filePath = '1.txt'
+
+const readFileLines = filename =>
+   fs.readFileSync(filename)
+   .toString('UTF8')
+   .split('\n');
+
+let arr = readFileLines(filePath)
+
+const json = []
+
+const parentRegex = /^(\d+\.\s)+/g
+const childRegex = /^(\d\.\d\.\s)+/g
+let listNumber = 0
+let childCounter = 0
+let dashCounter = 0
+for (let i = 0; i < arr.length; i++) {
+  const firstChar = arr[i].split('.')[0];
+  const currentListNumber = !Number.isNaN(firstChar) ? +firstChar : listNumber;
+
+  if (arr[i].match(parentRegex)) {
+    childCounter = 0;
+    json.push({ id: listNumber, title: arr[i].replace(parentRegex, ''), description: [] });
+  } 
+
+  if (arr[i].match(childRegex)) {
+    dashCounter = 0;
+    json[listNumber - 1].description[childCounter] = {  id: childCounter + 1, title: arr[i].replace(childRegex, ''), description: []  };
+    childCounter++;
+  }
+
+  if (arr[i].startsWith('-')) {
+    // console.log('asd', childCounter, json[listNumber - 1].description[childCounter - 1].description);
+    json[listNumber - 1].description[childCounter - 1].description.push(arr[i].replace('- ', ''))
+    dashCounter++;
+  }
+
+  if (!Number.isNaN(currentListNumber) && currentListNumber !== listNumber) {
+    listNumber++;
+  }
+
+}
+
+fs.writeFile('./data2.json', JSON.stringify(json, null, ' '), function(err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('saved')
+})
